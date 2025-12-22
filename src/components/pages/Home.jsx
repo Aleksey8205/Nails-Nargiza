7d81179fb7f8213726119bae710158603b0b2c84
Aleksey8205 <Aleksey8205@yandex.ru>
2025-12-22 17:23:27 +0300

    update

--- src/components/pages/Home.jsx
+++ src/components/pages/Home.jsx
@@ -1,32 +1,36 @@
 import React from "react";
 import video from "../../public/images/nailsVideo.mp4";
-import face from "../../public/images/main-image.jpg"
+import face from "../../public/images/face-image.jpg"
 import "../../shared/home.css";
 
 import HomeAbout from "./componentPages/HomeAbout.jsx";
 import HomePortfolio from "./componentPages/HomePortfolio.jsx";
 
 const Home = () => {
+
+  const handleTelegramOpen = () => {
+    window.location.href = `https://t.me/+79945550006`;
+  };
   return (
     <>
       <div className="main">
         <div className="video-container">
-            <div className="text-container">
+          <div className="text-container">
             <h2>Имя Фамилия</h2>
             <p className="text-beauty">Лучший бьюти мастер</p>
-            </div>
+          </div>
           <video className="video" autoPlay loop muted playsInline>
             <source src={video} type="video/mp4" />
             Your browser does not support the video tag.
           </video>
-          <button className="video-button">Записаться</button>
+          <button onClick={handleTelegramOpen} className="video-button">Записаться</button>
         </div>
         <div>
-            <img className="image-face" src={face} alt="" />
+          <img className="image-face" src={face} alt="" />
         </div>
       </div>
-      <HomeAbout/>
-      <HomePortfolio/>
+      <HomeAbout />
+      <HomePortfolio />
     </>
   );
 };
--- src/components/pages/componentPages/HomeAbout.jsx
+++ src/components/pages/componentPages/HomeAbout.jsx
@@ -1,18 +1,33 @@
 import React from "react";
-import face from "../../../public/images/main-image.jpg";
+import faces from "../../../public/images/face-image.jpg"
+
 import "./component-style/aboutHome.css";
 
 const HomeAbout = () => {
   return (
     <>
       <section className="about-section">
-        <img className="image-about" src={face} alt="" />
+        <img className="image-about" src={faces} alt="" />
         <div>
           <h2 className="caption-about">Немного о себе</h2>
-          <p className="text-about">Здесь должен быть текст как в примере </p>
-          <p className="text-about">например "Я мастер по маникюру"</p>
-          <p className="text-about">"Занимаюсь тем то тем то"</p>
-          <p className="text-about">и еще что то или больше блоков можно</p>
+          <p  className="text-about">Rакое то описание возможно было бы</p>
+          <p  className="text-about">какое то описание возможно было бы</p>
+          <p  className="text-about">какое то описание возможно было бы</p>
+          <div className="social-item">
+            <a className="link_social" href="https://www.instagram.com/nogti_podolsk5?igsh=MW5scGlycWIwbW53Zg%3D%3D&utm_source=qr">
+              <p className="social-icon social-instagram">Instagram</p>
+              <p>nogti_podolsk5</p>
+            </a>
+            <a className="link_social" href="https://t.me/+79945550006">
+              <p className="social-icon social-telegram">Telegram</p>
+              <p>@Cobr006</p>
+            </a>
+            <a className="link_social" href={`tel:+79945550006 `}>
+              <p className="social-icon social-phone">Номер телефона</p>
+              <p>+7(994)-555-00-06</p>
+            </a>
+          </div>
+
         </div>
       </section>
     </>
