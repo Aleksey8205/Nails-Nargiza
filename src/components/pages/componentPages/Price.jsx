d8ce6c794b977afa6bab3feb7af6df5bf34222b7
Alexey Danilov <Aleksey8205@yandex.ru>
2026-01-02 04:00:01 +0300

    update

--- src/components/pages/componentPages/Price.jsx
+++ src/components/pages/componentPages/Price.jsx
@@ -10,31 +10,31 @@ const Price = () => {
       <div className="nullBox"></div>
     <div className="service-box">
         <h3 className="caption-price">Маникюр</h3>
-        <p class="service-item">Маникюр без покрытия <span class="price">800</span></p>
-        <p class="service-item">Маникюр с покрытием <span class="price">1500</span></p>
-        <p class="service-item">Укрепление твердым материалом(гель, полигель) <span class="price">500</span></p>
-        <p class="service-item">Френч <span class="price">200</span></p>
-        <p class="service-item">Втирка, блёстки, фольга, стемпинг <span class="price">50</span></p>
-        <p class="service-item">Снятие без покрытия <span class="price">300</span></p>
-        <p class="service-item">Снятие нарощенных ногтей <span class="price">400</span></p>
+        <p className="service-item">Маникюр без покрытия <span className="price">800</span></p>
+        <p className="service-item">Маникюр с покрытием <span className="price">1500</span></p>
+        <p className="service-item">Укрепление твердым материалом(гель, полигель) <span className="price">500</span></p>
+        <p className="service-item">Френч <span className="price">200</span></p>
+        <p className="service-item">Втирка, блёстки, фольга, стемпинг <span className="price">50</span></p>
+        <p className="service-item">Снятие без покрытия <span className="price">300</span></p>
+        <p className="service-item">Снятие нарощенных ногтей <span className="price">400</span></p>
     </div>
 
     <div className="service-box">
         <h3 className="caption-price">Педикюр</h3>
-        <p class="service-item">Аппаратный педикюр <span class="price">900</span></p>
-        <p class="service-item">Аппаратный педикюр с покрытием <span class="price">1800</span></p>
-        <p class="service-item">Аппаратный педикюр с обработкой стоп и покрытие <span class="price">2100</span></p>
-        <p class="service-item">Наращивание и покрытие <span class="price">1800</span></p>
-        <p class="service-item">Ремонт / донаращивание <span class="price">100 / 50</span></p>
-        <p class="service-item">Коррекция нарощенных ногтей <span class="price">1800</span></p>
+        <p className="service-item">Аппаратный педикюр <span className="price">900</span></p>
+        <p className="service-item">Аппаратный педикюр с покрытием <span className="price">1800</span></p>
+        <p className="service-item">Аппаратный педикюр с обработкой стоп и покрытие <span className="price">2100</span></p>
+        <p className="service-item">Наращивание и покрытие <span className="price">1800</span></p>
+        <p className="service-item">Ремонт / донаращивание <span className="price">100 / 50</span></p>
+        <p className="service-item">Коррекция нарощенных ногтей <span className="price">1800</span></p>
     </div>
 
     <div className="service-box box-2">
         <h3 className="caption-price">Покрытие</h3>
-        <p class="service-item">Скраб для рук <span class="price">150</span></p>
-        <p class="service-item">Парафин <span class="price">399</span></p>
-        <p class="service-item">Массаж для рук <span class="price">200</span></p>
-        <p class="service-item">Эпиляция рук <span class="price">200</span></p>
+        <p className="service-item">Скраб для рук <span className="price">150</span></p>
+        <p className="service-item">Парафин <span className="price">399</span></p>
+        <p className="service-item">Массаж для рук <span className="price">200</span></p>
+        <p className="service-item">Эпиляция рук <span className="price">200</span></p>
     </div>
     </div>
 </section>
--- src/components/pages/componentPages/ShedulePlain.jsx
+++ src/components/pages/componentPages/ShedulePlain.jsx
@@ -1,14 +1,33 @@
 import React from "react";
-
+import "./component-style/shedulePlain.css"
 
 const ShedulePlain = () => {
-     return (
-        <>
-        <section>
-            <h2 className="caption-portfolio">Условия работы</h2>
-        </section>
-        </>
-     )
-}
+  return (
+    <>
+      <section>
+        <h2 className="caption-portfolio">Условия работы</h2>
+        <div className="back-pint"></div>
+        <div className="shedule">
+
+            <section className="schedule-item">
+              <h3 className="caption-price">График</h3>
+              <div className="chart">
+              <p>Пн-Пт с 9.00-21.00</p>
+                <p>Сб-Вс с 11.00-22.00</p>
+              </div>
+            </section>
+
+            <section className="schedule-item">
+              <h3 className="caption-price">Важно</h3>
+              <div className="chart">
+               <p>Если вы опоздали на запись, по предварительной записи можно на другое свободное окно</p>
+                {/* <p>Для некоторых клиентов, на первое посещение действует скидка 20%</p>  */}
+            </div>
+            </section>
+        </div>
+      </section>
+    </>
+  );
+};
 
 export default ShedulePlain;
