ba24437637eb6b1c16e9e6ef9e71116da15519a5
Alexey Danilov <Aleksey8205@yandex.ru>
2026-01-02 14:54:40 +0300

    update

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,9 +1,30 @@
 import React from "react";
+import "../shared/footer.css";
+import insta from "../public/images/instagram.png";
+import telegram from "../public/images/telegram.png";
+import phone from "../public/images/phone.png"
 
 const Footer = () => {
   return (
     <>
-      <h3 className="caption-portfolio">Контакты</h3>
+      <footer className="footer">
+        <h3 className="caption-portfolio">Контакты</h3>
+        
+
+        <div className="social-item">
+          <a
+            className="link_social"
+            href="https://www.instagram.com/nogti_podolsk5?igsh=MW5scGlycWIwbW53Zg%3D%3D&utm_source=qr">
+            <img className="social-icon" src={insta} alt="" />
+          </a>
+          <a className="link_social" href="https://t.me/+79945550006">
+          <img className="social-icon" src={telegram} alt="" />
+          </a>
+          <a className="link_social" href={`tel:+79945550006 `}>
+            <img className="social-icon" src={phone} alt="" />
+          </a>
+        </div>
+      </footer>
     </>
   );
 };
--- src/components/pages/componentPages/HomeAbout.jsx
+++ src/components/pages/componentPages/HomeAbout.jsx
@@ -1,5 +1,5 @@
 import React from "react";
-import faces from "../../../public/images/face-image.jpg"
+import faces from "../../../public/images/face-image.jpg";
 
 import "./component-style/aboutHome.css";
 
@@ -10,24 +10,13 @@ const HomeAbout = () => {
         <img className="image-about" src={faces} alt="" />
         <div>
           <h2 className="caption-about">Немного о себе</h2>
-          <p  className="text-about">Я занимаюсь маникюром более 2-х лет</p>
-          <p  className="text-about">Для красивого маникюра я использую только качественые материалы</p>
-          <p  className="text-about">работаю как аппаратным так и  любым другим?</p>
-          {/* <div className="social-item">
-            <a className="link_social" href="https://www.instagram.com/nogti_podolsk5?igsh=MW5scGlycWIwbW53Zg%3D%3D&utm_source=qr">
-              <p className="social-icon social-instagram">Instagram</p>
-              <p className="addr">nogti_podolsk5</p>
-            </a>
-            <a className="link_social" href="https://t.me/+79945550006">
-              <p className="social-icon social-telegram">Telegram</p>
-              <p className="addr">@Cobr006</p>
-            </a>
-            <a className="link_social" href={`tel:+79945550006 `}>
-              <p className="social-icon social-phone">Телефон</p>
-              <p className="addr">+7(994)-555-00-06</p>
-            </a>
-          </div> */}
-
+          <p className="text-about">Я занимаюсь маникюром более 2-х лет</p>
+          <p className="text-about">
+            Для красивого маникюра я использую только качественые материалы
+          </p>
+          <p className="text-about">
+            работаю как аппаратным так и любым другим?
+          </p>
         </div>
       </section>
     </>
